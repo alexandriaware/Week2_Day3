e5c3bfcc602644ce897a5c9e6f2ea13c8ba11ac9
alexandriaware <ali.mercer92@gmail.com>
2017-10-05 13:03:47 -0400

    Class Practice Methods

--- Week2_Day3/Program.cs
+++ Week2_Day3/Program.cs
@@ -10,16 +10,92 @@ namespace Week2_Day3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Add(5,7));
+            ////Practice Problem One
+            //FavoriteFoody("Bob", "Pizza");
+            //FavoriteFoody("Mia", "Ice Cream");
+            //FavoriteFoody("Beth", "Apples");
+
+            //Console.WriteLine("Please enter a name");
+            //string userName = Console.ReadLine();
+
+            //Console.WriteLine("Please enter the best food ever!");
+            //string userFood = Console.ReadLine();  
+
+
+            //Practice Problem Two
+            RetirementCalculator(24);
+
+
+
+
         }
 
-
-        //methods go outside Main, but on the same level
-        public static int Add(int firstNumber, int secondNumber)
+        public static void RetirementCalculator(int userAge)
         {
-            int sum = firstNumber + secondNumber;
+            int retirementAge = Math.Max(0, 65 - userAge);
+            //Math.Max(); returns the greater of two integers
 
-            return sum;
+            Console.WriteLine("The user will retire in " + retirementAge + " years");
         }
+
+
+
+
+
+        public static void FavoriteFoody(string name, string food)
+        {
+            Console.WriteLine("************************************");
+            Console.WriteLine(name + "'s favorite food is  " + food);
+            Console.WriteLine();
+        }
+
+    
+
+        public static void RetirementCalc(int userAge)
+        {
+            Console.WriteLine("Please enter your age");
+            int age = int.Parse(Console.ReadLine());
+            int retirement = 65 - age;
+
+            Console.WriteLine("The user will retire in " + retirement + "years.");
+        }
+
+
+
+        public static double WageCalculator(double hoursWorked, double hourlyWage)
+        {
+            Console.WriteLine("Please enter how many hours you've worked this month");
+            double hours = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter how much you make per hour");
+            double wage = double.Parse(Console.ReadLine());
+            double montlyWage = wage * hours;
+            Console.WriteLine(montlyWage);
+            return montlyWage;
+        }
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ////methods go outside Main, but on the same level
+        //public static int Add(int firstNumber, int secondNumber)
+        //{
+        //    int sum = firstNumber + secondNumber;
+
+        //    return sum;
+        //}
     }
 }
